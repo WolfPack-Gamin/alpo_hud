3644de1c0d48805f38f8fea31c5bd8ce85dbecc0
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-02-08 17:45:20 -0800

    Initial commit

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.Locale = "en"
+
+Config.Fahrenheit = true
+
+-- Sensitive data: Modify only under your responsability
+
+-- Celsius: 
+Config.ExtremeColdC = -5
+Config.ColdC = 5
+Config.CoolC = 5
+Config.WarmC = 20
+Config.HotC = 25
+Config.ExtremeHotC = 30
+
+-- Fahrenheit: 
+Config.ExtremeColdF = 23
+Config.ColdF = 41
+Config.CoolF = 50
+Config.WarmF = 68
+Config.HotF = 77
+Config.ExtremeHotF = 86
+
+Config.LittleStaminaReduction = 2
+Config.MediumStaminaReduction = 3
+Config.LargeStaminaReduction = 5
+Config.LittleWaterReduction = 2
+Config.MediumWaterReduction = 3
+Config.LargeWaterReduction = 3
